c29c9df03e45687cf84ea7dc6ae54b8a14db8afe
Henry-Santa <98135968+Henry-Santa@users.noreply.github.com>
2025-03-01 11:34:31 -0500

    Minor changes

--- src/Features.js
+++ src/Features.js
@@ -10,13 +10,13 @@ function Features() {
           <img src="/images/Henry.png" alt="Henry" className="feature-image" loading="lazy"/>
           <h3>Henry Santangelo</h3>
           <h4>Founder</h4>
-          <p>Henry been programming since 2020 and became interested in the subject during the pandemic. When he was younger, he felt a lack of resources present in the school system to learn computer science. With that knowledge, he decided to found Kids Teaching Code to help fill the resource gap that students passionate in computer science might feel.</p>
+          <p>Henry has been programming since 2020 and became interested in the subject during the pandemic. When he was younger, he felt a lack of resources present in the school system to learn computer science. With that knowledge, he decided to found Kids Teaching Code to help fill the resource gap that students passionate in computer science might feel.</p>
         </div>
         <div className="feature-item">
           <img src="/images/Zach.png" alt="Zach" className="feature-image zach" loading="lazy"/>
           <h3>Zach Bostox</h3>
           <h4>Co-Founder</h4>
-          <p>As well as a programmer, Zach is a comitted theater student. He became interested in programming during his first year of Computer Science at Greenwich Country Day School. Through his passion for the subject, he has honed his skills over the past few years and wants to teach younger kids the value of coding.</p>
+          <p>As well as a programmer, Zach is a committed theater student. He became interested in programming during his first year of Computer Science at Greenwich Country Day School. Through his passion for the subject, he has honed his skills over the past few years and wants to teach younger kids the value of coding.</p>
         </div>
       </div>
     </div>
--- src/Footer.js
+++ src/Footer.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
+import './Footer.css'; // Import the CSS file
 
 function Footer() {
   return (
-    <footer className="bg-dark text-white mt-5 p-4 text-center">
+    <footer className="footer bg-dark text-white mt-5 p-4 text-center">
       <Container>
         <Row>
           <Col>
